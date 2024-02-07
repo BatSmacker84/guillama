662e46df5f42ad822ed243de9d8c3823d55c6dc9
BatSmacker84 <ooohhhwells@gmail.com>
2024-02-07 09:21:02 -0600

    slint hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        Text {
+            text: "Hello World!";
+            color: black;
+        }
+    }
 }
